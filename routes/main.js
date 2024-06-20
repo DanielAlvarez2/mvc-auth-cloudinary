1b1fdd97b189137cc19c11516f8f3e1cb6f3545b
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-06-20 11:31:42 -0400

    fixed typo missing / in logout route in routes/main.js

--- routes/main.js
+++ routes/main.js
@@ -11,7 +11,7 @@ router.get('/profile',ensureAuth,postsController.getProfile)
 router.get('/feed',ensureAuth,postsController.getFeed)
 router.get('/login',authController.getLogin)
 router.post('/login',authController.postLogin)
-router.get('logout',authController.logout)
+router.get('/logout',authController.logout)
 router.get('/signup',authController.getSignup)
 router.post('/signup',authController.postSignup)
 
